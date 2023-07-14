cb48217e56f5d6e3f1b7e8e9e7f881381f44e2d2
Prateek Vernekar <mprateekvernekar189@gmail.com>
2023-07-14 17:42:04 +0530

    Changes in signin frontend

--- react_app/src/Register/SignerType.jsx
+++ react_app/src/Register/SignerType.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import OrgSignin from './OrgSignin';
+import FanSignin from './FanSignin';
 
 const SignerType = () => {
   const navigate = useNavigate();
 
-
   const handleSelect=(e)=>{
     if(e.target.name==='fan'){
-      navigate('/fansignin');
+      navigate('/fansignin',{state:{type:e.target.name}});
     }
     else{
-      navigate('/orgsignin');
+      navigate('/orgsignin',{state:{type:e.target.name}});
     }
   }
 
@@ -20,7 +21,7 @@ const SignerType = () => {
         <div className='text-center text-2xl my-3'>Register As</div>
         <button className="w-1/2 mx-auto my-5 text-xl font-serif flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-indigo-500 to-purple-400 hover:from-indigo-600 hover:to-purple-500 active:from-indigo-700 active:to-purple-600 focus-visible:ring ring-indigo-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-4" name="manager"  onClick={handleSelect}>Event Manager</button>
         <button className="w-1/2 mx-auto my-5 text-xl font-serif flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-indigo-500 to-purple-400 hover:from-indigo-600 hover:to-purple-500 active:from-indigo-700 active:to-purple-600 focus-visible:ring ring-indigo-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-4" name="coach" onClick={handleSelect}>Coach</button>
-        <button className="w-1/2 mx-auto my-5 text-xl font-serif flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-indigo-500 to-purple-400 hover:from-indigo-600 hover:to-purple-500 active:from-indigo-700 active:to-purple-600 focus-visible:ring ring-indigo-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-4" name="athelete" onClick={handleSelect}>Athelete</button>
+        <button className="w-1/2 mx-auto my-5 text-xl font-serif flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-indigo-500 to-purple-400 hover:from-indigo-600 hover:to-purple-500 active:from-indigo-700 active:to-purple-600 focus-visible:ring ring-indigo-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-4" name="athlete" onClick={handleSelect}>Athelete</button>
         <button className="w-1/2 mx-auto my-5 text-xl font-serif flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-indigo-500 to-purple-400 hover:from-indigo-600 hover:to-purple-500 active:from-indigo-700 active:to-purple-600 focus-visible:ring ring-indigo-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-4" name="fan" onClick={handleSelect}>Fan</button>
       </div>
     </>
